3b0a2f18c82d4fbe18ee81fba60fe16290f487db
vidisha.shah25 <vidisha.shah14@gmail.com>
2024-01-12 14:16:41 +0100

    Navbar fixed

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -15,10 +15,10 @@ export function LanguageToggle() {
 
   return (
     <nav className="bg-neutral-800">
-    <div className="container mx-auto px-4">
-      <div className="flex justify-between items-center py-4">
+    <div className=" px-10">
+      <div className="flex justify-between items-center py-4 w-full">
         {!isOpen && (
-          <a href="#" className="text-xl font-bold text-white w-fit h-fit ml-10">
+          <a href="#" className="text-xl font-bold text-white w-fit h-fit">
             <svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 640 512">
               <path fill="#25b1a5" d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
             </svg>
@@ -39,12 +39,12 @@ export function LanguageToggle() {
             isOpen ? "block" : "hidden"
           } md:flex md:items-center md:w-auto w-full`}
         >
-          <ul className={`md:flex items-center ${isOpen ? "justify-center" : ""}`}>
+          <ul className={`md:flex items-center ${isOpen ? "justify-center items-center align-middle mx-auto" : ""}`}>
             
             <li className="ml-3">
               <a
                 href="#about"
-                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-md  font-medium"
               >
                 About
               </a>
@@ -52,7 +52,7 @@ export function LanguageToggle() {
             <li className="ml-3">
               <a
                 href="#skills"
-                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-md  font-medium"
               >
                 Skills
               </a>
@@ -60,7 +60,7 @@ export function LanguageToggle() {
             <li className="ml-3">
               <a
                 href="#project"
-                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-md  font-medium"
               >
                 Projects
               </a>
@@ -68,13 +68,13 @@ export function LanguageToggle() {
             <li className="ml-3">
               <a
                 href="#contact"
-                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-md font-medium"
               >
                 Contact
               </a>
             </li>
             <li className="ml-3">
-            <select value={i18n.language} onChange={handleLanguageChange}  >
+            <select value={i18n.language} onChange={handleLanguageChange} className="bg-neutral-900 text-white font-semibold px-3 py-2 items-center align-middle text-md rounded-lg" >
               <option value="en">en</option>
               <option value="de">de</option>
             </select>
